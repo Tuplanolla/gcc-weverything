71cd4511c56999c68ec2bf5f0a7860bdfb95f225
Sampsa Kiiskinen <tuplanolla@gmail.com>
2016-03-24 18:54:26 +0200

    Inspiring commit message.

--- checkout.sh
+++ checkout.sh
@@ -7,27 +7,27 @@
 # should specify where the source code repository is.
 
 test "$#" -eq 1 || {
-	echo "Argument not found."
-	exit 1
+  echo "Argument not found."
+  exit 1
 }
 
 test -d "$1" -a -d "$1/.git" || {
-	echo "Repository not found."
-	exit 1
+  echo "Repository not found."
+  exit 1
 }
 
 command -v csi > /dev/null && test -x extract.scm || {
-	echo "Runtime not found."
-	exit 1
+  echo "Runtime not found."
+  exit 1
 }
 
 mkdir -p tags || {
-	echo "Directory not found."
-	exit 1
+  echo "Directory not found."
+  exit 1
 }
 
 for t in `git --git-dir "$1/.git" tag | tac`
 do
-	git --git-dir "$1/.git" --work-tree "$1" checkout "$t" \
-			&& ./extract.scm "$1" > "tags/$t"
+  git --git-dir "$1/.git" --work-tree "$1" checkout "$t" \
+    && ./extract.scm "$1" > "tags/$t"
 done
